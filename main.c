86ee2e8993b11c010eb8de3a8b0c10ce4b8acbc5
Anokhautomation <67797819+anokhramesh@users.noreply.github.com>
2022-07-13 20:08:03 +0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+//Print Name with ASCII
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int main()
+{   printf("Press any key run the program\n");
+    getch();
+    printf("\n%c %c %c %c %c ",65,78,79,75,72);
+    printf("%c %c %c %c %c %c\n",82,65,77,69,83,72);
+    getch();
+    return 0;
+}
